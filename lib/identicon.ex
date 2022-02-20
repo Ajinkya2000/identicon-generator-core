5108d3fe1706ab2b47e584e92ee0d679f40b8dbd
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2022-02-20 15:57:38 +0530

    create a new property for struct and create a grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,13 +9,37 @@ defmodule Identicon do
 
   def pick_color(image) do
     %Identicon.Image{hex: hex_list} = image
+
+    # Get first 3 values from the list
     [r, g, b | _tail] = hex_list
-    [r, g, b]
+
+    # Create a new struct with all properties from above struct + color property
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def main(input) do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 end
